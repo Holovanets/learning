66555adbd83d1b3eb3fa0a07090b6a883016e814
Holovanets <vip.golovanets@gmail.com>
2022-07-05 13:22:30 +0300

    Added sort

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import PostForm from './components/PostForm.jsx'
 import PostItem from './components/PostItem.jsx'
 import PostList from './components/PostList.jsx'
 import MyButton from './components/UI/button/MyButton.jsx'
 import MyInput from './components/UI/input/MyInput.jsx'
+import MySelect from './components/UI/select/MySelect.jsx'
 
 import './styles/App.css'
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'JavaScript 2', body: 'Description' },
-    { id: 3, title: 'JavaScript 3', body: 'Description' },
-    { id: 4, title: 'JavaScript 4', body: 'Description' },
+    { id: 1, title: 'аа', body: 'сс' },
+    { id: 2, title: 'ыы 2', body: 'аа' },
+    { id: 3, title: 'цц 3', body: 'рр' },
+    { id: 4, title: 'ее 4', body: 'аа' },
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
 
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList posts={posts} title="Список постов" />
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
+
+      {posts.length ? (
+        <PostList remove={removePost} posts={posts} title="Список постов" />
+      ) : (
+        <h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
+      )}
     </div>
   )
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MyButton from './UI/button/MyButton'
 
 const PostItem = (props) => {
   return (
@@ -11,7 +12,7 @@ const PostItem = (props) => {
           <div>{props.post.body}</div>
         </div>
         <div className="post__btns">
-          <button>Удалить</button>
+          <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
         </div>
       </div>
     </div>
