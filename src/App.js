802706acf7c57ccdf523b5bce2a22d110a7eac92
Holovanets <vip.golovanets@gmail.com>
2022-07-15 17:40:40 +0300

    Added loader and fetch request

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
+import axios from 'axios'
 import React, { useState, useMemo } from 'react'
+import { useEffect } from 'react'
+import PostService from './components/API/PostService.js'
 import PostFilter from './components/PostFilter.jsx'
 import PostForm from './components/PostForm.jsx'
 import PostItem from './components/PostItem.jsx'
 import PostList from './components/PostList.jsx'
 import MyButton from './components/UI/button/MyButton.jsx'
 import MyInput from './components/UI/input/MyInput.jsx'
+import Loader from './components/UI/Loader/Loader.jsx'
+import MyModal from './components/UI/MyModal/MyModal.jsx'
 import MySelect from './components/UI/select/MySelect.jsx'
+import { useFetching } from './hooks/useFetching.js'
+import { usePosts } from './hooks/usePosts.js'
 
 import './styles/App.css'
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'аа', body: 'сс' },
-    { id: 2, title: 'ыы 2', body: 'аа' },
-    { id: 3, title: 'цц 3', body: 'рр' },
-    { id: 4, title: 'ее 4', body: 'аа' },
-  ])
-
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({ sort: '', query: '' })
-  // const [selectedSort, setSelectedSort] = useState('')
-  // const [searchQuery, setSearchQuery] = useState('')
-
-  const sortedPosts = useMemo(() => {
-    console.log('Отработала функция поиска')
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      )
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    )
-  }, [filter.query, sortedPosts])
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [fetchPosts, isPostsLading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll()
+    setPosts(posts)
+  })
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
@@ -47,19 +40,33 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
       <hr style={{ margin: '15px 0' }} />
 
       <PostFilter filter={filter} setFilter={setFilter} />
-
-      {sortedAndSearchedPosts.length ? (
+      {postError && <h1>Произошла ощшибка ${postError}</h1>}
+      {isPostsLading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '50px',
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
         <PostList
           remove={removePost}
           posts={sortedAndSearchedPosts}
           title="Список постов"
         />
-      ) : (
-        <h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
       )}
     </div>
   )
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,24 @@
+import { useMemo } from 'react'
+
+export const useSortedPosts = (posts, sort) => {
+  const sortedPosts = useMemo(() => {
+    console.log('Отработала функция поиска')
+    if (sort) {
+      return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]))
+    }
+    return posts
+  }, [sort, posts])
+  return sortedPosts
+}
+
+export const usePosts = (post, sort, query) => {
+  const sortedPosts = useSortedPosts(post, sort)
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(query.toLowerCase())
+    )
+  }, [query, sortedPosts])
+
+  return sortedAndSearchedPosts
+}
